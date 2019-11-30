3845cc83ef4d3b9e69ba6431747ea1c77b868497
DanilVod <43678372+DanilVod@users.noreply.github.com>
2019-11-30 12:36:34 +0300

    Оптимизировал локалсторейдж

--- src/index.js
+++ src/index.js
@@ -16,14 +16,14 @@ class Table {
     let unicode = 65;
     //создание колонок
     for (let k = 1; k < this.colsCount + 1; k++) {
-      firstCol.push(`<div class="first row-col" data-col='${k}'>${String.fromCodePoint(unicode)}
+      firstCol.push(`<div class="first row-col" style="width: ${localStorage.getItem(`width${k}`)};" data-col='${k}'>${String.fromCodePoint(unicode)}
       <div class="resizerCol"></div>
       </div>`)
       unicode++
     }
     //создание строк
     for (let j = 0; j < this.colsCount; j++) {
-      cols.push(`<div class="row-col" contenteditable="true" data-col='${j + 1}'></div>`)
+      cols.push(`<div class="row-col" contenteditable="true" style="width: ${localStorage.getItem(`width${j + 1}`)};" data-col='${j + 1}'></div>`)
     }
     rows.push(`<div class="row">
         <div class="row-info">
@@ -34,7 +34,7 @@ class Table {
       </div>`)
     for (let i = 0; i < this.rowsCount; i++) {
       rows.push(`<div class="row">
-        <div class="row-info" data-row="${i + 1}">${i + 1}
+        <div class="row-info" style="height: ${localStorage.getItem(`height${i + 1}`)};" data-row="${i + 1}">${i + 1}
         <div class="resizerRow"></div>
         </div>
         <div class="row-content">
@@ -61,12 +61,10 @@ function makeResize() {
       //Функция ресайза строк
       let cordsRow = parentRow.getBoundingClientRect()
       document.onmousemove = function (e) {
-        let rows = document.querySelectorAll(`[data-row="${parentRow.dataset.row}"]`)
-        let rowNumber = rows[0].dataset.row
         let deltaRow = e.pageY - cordsRow.bottom
         let height = cordsRow.height + deltaRow + 'px'
         parentRow.style.height = height
-        localStorage.setItem(`height${rowNumber}`, `${height}`)
+        localStorage.setItem(`height${parentRow.dataset.row}`, `${height}`)
       }
     }
     if (parentCol) {
@@ -74,12 +72,11 @@ function makeResize() {
       let cols = document.querySelectorAll(`[data-col="${parentCol.dataset.col}"]`)
       let cordsCol = parentCol.getBoundingClientRect()
       document.onmousemove = function (e) {
-        let colNumber = cols[0].dataset.col
         let deltaCol = e.pageX - cordsCol.right
         let width = cordsCol.width + deltaCol + 'px'
         parentCol.style.width = width
         cols.forEach((col) => col.style.width = width)
-        localStorage.setItem(`width${colNumber}`, `${width}`)
+        localStorage.setItem(`width${parentCol.dataset.col}`, `${width}`)
       }
     }
     document.onmouseup = function () {
@@ -88,26 +85,3 @@ function makeResize() {
   });
 }
 makeResize()
-//localstorage
-function doLocalStorage() {
-  let rowsCount = document.querySelectorAll('.row-info')
-  for (let i = 0; i < rowsCount.length; i++) {
-
-    if (localStorage.getItem(`height${i}`) !== null) {
-      rowsCount[i].style.height = localStorage.getItem(`height${i}`)
-    }
-  }
-  let colsCount = document.querySelectorAll('.first')
-  let cols1 = document.querySelectorAll(`.row-col`)
-
-  for (let i = 0; i < colsCount.length; i++) {
-    if (localStorage.getItem(`width${i}`) !== null) {
-      cols1.forEach((el) => {
-        if (el.dataset.col == i) {
-          el.style.width = localStorage.getItem(`width${i}`)
-        }
-      })
-    }
-  }
-}
-doLocalStorage()
